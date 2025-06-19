e5286f042afa3bdb13d8961d51b54d02f9a2520b
ayushsingh41 <ayushgaharwar77@gmail.com>
2025-06-19 23:40:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
